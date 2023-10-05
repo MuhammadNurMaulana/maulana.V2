a4920e184a13aaa32c9ff2eac8a65c0019d48d14
“MuhammadNurMaulana” <muhammadnrmaulana@gmail.com>
2023-10-05 11:45:51 +0700

    create about,article,blog pages and uppdate navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Projects } from "./pages/Projects";
 import { Navbar } from "./components/Navbar";
+import { Blog } from "./pages/Blog";
+import { Article } from "./pages/Article";
+import { About } from "./pages/About";
 function App() {
   return (
     <>
@@ -9,6 +12,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/blogs" element={<Blog />} />
+        <Route path="/articles" element={<Article />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </>
   );
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,11 @@
 import { HiHomeModern } from "react-icons/hi2";
+import { FaHornbill, FaGg, FaInnosoft } from "react-icons/fa";
+import { AiOutlineHome } from "react-icons/ai";
+import { GrProjects } from "react-icons/gr";
 import { useState } from "react";
 import { ContainerNavbar } from "./ContainerNavbar";
 import { NavbarHeader } from "./NavbarHeader";
+import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -9,6 +13,34 @@ export const Navbar = () => {
   const showNavbar = () => {
     setIsOpen(!isOpen);
   };
+
+  const navMenu = [
+    {
+      name: "Home",
+      link: "/",
+      icon: <AiOutlineHome />,
+    },
+    {
+      name: "Projects",
+      link: "/projects",
+      icon: <GrProjects />,
+    },
+    {
+      name: "About",
+      link: "/about",
+      icon: <FaHornbill />,
+    },
+    {
+      name: "Blog",
+      link: "/blogs",
+      icon: <FaGg />,
+    },
+    {
+      name: "Article",
+      link: "/articles",
+      icon: <FaInnosoft />,
+    },
+  ];
   return (
     <ContainerNavbar>
       <div className="group">
@@ -18,8 +50,27 @@ export const Navbar = () => {
         </button>
       </div>
 
-      <div className="bg-slate-300 px-8 flex-col flex gap-8 rounded-lg border-b border-slate-800">
+      <div className="bg-slate-300 px-8 py-4 flex-col flex gap-8 rounded-xl">
         <NavbarHeader isOpen={`${isOpen ? "flex" : "lg:hidden"}`} setIsOpen={showNavbar} />
+
+        <div className="p-2 flex-col flex gap-4 text-xl border-b border-t border-slate-800">
+          {navMenu.map((item, index) => (
+            <NavLink
+              to={item.link}
+              key={index}
+              className={({ isActive }) => (isActive ? "bg-slate-100 flex items-center gap-2 py-2 px-3 rounded-lg translate-x-1 duration-300" : "flex items-center gap-2 py-2 px-3 transition-all duration-300")}
+            >
+              {item.icon}
+              <h1 className={`font-mono  font-semibold ${isOpen ? "flex" : "lg:hidden"}`}>{item.name}</h1>
+            </NavLink>
+          ))}
+        </div>
+
+        <footer className={`font-mono text-sm font-semibold ${isOpen ? "flex" : "lg:hidden"}`}>
+          <h1>
+            &copy; 2023 with <span className="animate-pulse">❤️</span> by Maulana
+          </h1>
+        </footer>
       </div>
     </ContainerNavbar>
   );
